06fff1aebab328ec035539f22107847ffe053af9
noerhdy <hidayatnoer11@gmail.com>
2024-06-06 19:20:51 +0700

    on progress

--- src/pages/CreateBlog/CreateBlog.jsx
+++ src/pages/CreateBlog/CreateBlog.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FormCreate from "../../components/Fragment/FormCreate";
+import CtaBack from "@/components/Elements/Button/CtaBack";
 
 const CreateBlog = () => {
   return (
     <section className=" flex flex-col items-center bg-[#f7f7f7] dark:bg-zinc-950">
       <div className="w-full max-w-screen-md text-balance">
         <div className="flex flex-col text-center justify-center relative">
+          <CtaBack link="/">Home</CtaBack>
           <img
             className="object-cover w-auto h-1/2 bg-center dark:opacity-5  "
             src="imageBlog/line-pattern.png"
--- src/pages/DetailBlog/DetailBlog.jsx
+++ src/pages/DetailBlog/DetailBlog.jsx
@@ -1,9 +1,11 @@
+import CtaBack from "@/components/Elements/Button/CtaBack";
 import React from "react";
 
 const DetailBlog = () => {
   return (
-    <section className=" flex justify-center min-h-screen items-center bg-[#f7f7f7] dark:bg-zinc-950 ">
+    <section className=" flex flex-col min-h-screen items-center bg-[#f7f7f7] dark:bg-zinc-950 ">
       <div className="w-full max-w-screen-md p-12 rounded-md space-y-6">
+        <CtaBack link="/">Back</CtaBack>
         <img
           className="object-cover sm:w-full sm:h-full w-auto aspect-video drop-shadow-md bg-center duration-500 ease-in-out rounded-lg "
           src="imageBlog/sec2.webp"
