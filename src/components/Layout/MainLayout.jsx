d04ed1128b62f86090262b7eca7611c6549c2bab
noerhdy <hidayatnoer11@gmail.com>
2024-06-18 21:07:48 +0700

    [GET] API memakai AXIOS

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
-import CardMain from '../Fragment/CardMain'
+import React from "react";
+import CardMain from "../Fragment/CardMain";
+import { useEffect } from "react";
+import Axios from "axios";
 
 const MainLayout = () => {
+  useEffect(() => {
+    Axios.get("http://localhost:4000/v1/blog/posts")
+      .then((result) => {
+        console.log("Data API", result.data);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }, []);
   return (
-  <section className=" flex flex-col min-h-screen items-center  bg-[#f7f7f7] dark:bg-zinc-950 ">
-  <div className="w-full max-w-screen-md bg-[#f7f7f7] dark:bg-zinc-950 text-balance">
-    <div className="flex flex-col text-center justify-center pb-8 relative items-center  ">
-      <img className="object-cover w-auto bg-center dark:opacity-5"
-      src="imageBlog/line-pattern.png"></img>
-      <div className="flex absolute flex-col space-y-4 w-1/2 ">
-        <h1 className="font-bold text-5xl text-black dark:text-white">Blog</h1>
-        <p className="text-xl text-black dark:text-zinc-600 text-pretty">Lorem ipsum dolor sit amet consectetur adipisicing eli</p>
+    <section className=" flex flex-col min-h-screen items-center  bg-[#f7f7f7] dark:bg-zinc-950 ">
+      <div className="w-full max-w-screen-md bg-[#f7f7f7] dark:bg-zinc-950 text-balance">
+        <div className="flex flex-col text-center justify-center pb-8 relative items-center  ">
+          <img
+            className="object-cover w-auto bg-center dark:opacity-5"
+            src="imageBlog/line-pattern.png"
+          ></img>
+          <div className="flex absolute flex-col space-y-4 w-1/2 ">
+            <h1 className="font-bold text-5xl text-black dark:text-white">
+              Blog
+            </h1>
+            <p className="text-xl text-black dark:text-zinc-600 text-pretty">
+              Lorem ipsum dolor sit amet consectetur adipisicing eli
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div className="max-w-screen-md  pb-12  text-balance space-y-6">
-  <div className="flex flex-wrap justify-center gap-2 py-2">
-     <CardMain/>
-     </div>
-     </div>
-</section>
-  )
-}
+      <div className="max-w-screen-md  pb-12  text-balance space-y-6">
+        <div className="flex flex-wrap justify-center gap-2 py-2">
+          <CardMain />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
