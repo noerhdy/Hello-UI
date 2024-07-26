f54acb3719aa61acb9eac5ed685322d84bee4621
noerhdy <hidayatnoer11@gmail.com>
2024-07-27 05:11:12 +0700

    change color text button view

--- src/components/Elements/Button/CtaCard.jsx
+++ src/components/Elements/Button/CtaCard.jsx
@@ -8,10 +8,10 @@ const CtaCard = (props) => {
   return (
     <button
       onClick={() => handleClick(link)}
-      className="flex group items-center dark:bg-zinc-200  dark:hover:bg-zinc-100 bg-zinc-900 text-zinc-200 dark:text-zinc-900 hover:bg-zinc-800 rounded-lg px-4 py-1"
+      className="flex group items-center   dark:bg-zinc-200  dark:hover:bg-zinc-100 bg-zinc-900   hover:bg-zinc-800 rounded-lg px-4 py-1"
     >
-      <h3 className=" font-medium text-[0.75rem]">{nameLink}</h3>
-      <ArrowUpRight size={16} />
+      <h3 className=" font-medium text-blue-500 text-[0.85rem]">{nameLink}</h3>
+      {/* <ArrowUpRight size={18} className="text-blue-500" /> */}
     </button>
   );
 };
--- src/components/Elements/Button/CtaLink.jsx
+++ src/components/Elements/Button/CtaLink.jsx
@@ -1,7 +1,7 @@
 import { ArrowUpRight } from "lucide-react";
 import React from "react";
 export const arrowHover =
-  "duration-200 origin-center rotate-45 opacity-0 ease-in-out group-hover:opacity-100 group-hover:transform-none mx-1";
+  "duration-200 origin-center rotate-45 text-blue-500 opacity-0 ease-in-out group-hover:opacity-100 group-hover:transform-none mx-1";
 const CtaLink = (props) => {
   const { nameLink, link } = props;
   const handleClick = (url) => {
@@ -12,7 +12,7 @@ const CtaLink = (props) => {
       onClick={() => handleClick(link)}
       className="flex group items-center group space-x-1 text-zinc-950 dark:text-zinc-50"
     >
-      <h3 className="  translate-x-5 group-hover:translate-x-0 ease-in-out duration-200 ">
+      <h3 className="  text-blue-500 translate-x-5 group-hover:translate-x-0 ease-in-out duration-200 ">
         {nameLink}
       </h3>
       <ArrowUpRight size={20} className={` ${arrowHover}`} />
